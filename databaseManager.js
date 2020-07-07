c47c1c40283db85c58b228d3b9731d644ee3ba4c
Yentis <yentl178@gmail.com>
2020-07-07 18:55:09 +0200

    add sigterm exit handler

--- databaseManager.js
+++ databaseManager.js
@@ -312,7 +312,7 @@ function exitHandler(options) {
 process.on('exit', exitHandler.bind(null,{ cleanup:true }));
 
 process.on('SIGINT', exitHandler.bind(null, { exit: true }));
-
+process.on('SIGTERM', exitHandler.bind(null, { exit: true }));
 process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
 process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
 
