58ada650a752f6e7aee0f7a15c7c423700bdccb7
Yentis <yentl178@gmail.com>
2023-04-20 20:04:11 +0200

    update 2

--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ import * as settingsService from './services/settingsService';
 import * as osuApiService from './services/osuApiService';
 import { getCountryScores } from './services/buildService';
 import { login } from './services/discordService';
-import http from 'http';
+import { http } from 'follow-redirects';
 
 const server = http.createServer((_, res) => {
   res.end('Hello, World!');
@@ -28,9 +28,7 @@ async function init(): Promise<void> {
   if (pingUrl) {
     setInterval(() => {
       console.log('Pinging...');
-      sendRequest(pingUrl).then(() => {
-        console.log('Pinged.');
-      }).catch(console.error);
+      sendRequest(pingUrl);
     }, 15 * 60 * 1000);
   }
 
@@ -43,30 +41,49 @@ async function init(): Promise<void> {
   await checkToken();
 }
 
-async function sendRequest(url: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    http.get(url, (res) => {
-      res.on('error', (error) => {
-        reject(error);
-      });
+function sendRequest(url: string) {
+  const options = {
+    method: 'GET',
+    hostname: url,
+    path: '/',
+    headers: {
+      authority: url,
+      accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+      'accept-language': 'en-GB,en;q=0.9',
+      'cache-control': 'no-cache',
+      pragma: 'no-cache',
+      'sec-ch-ua': '"Chromium";v="112", "Brave";v="112", "Not:A-Brand";v="99"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"Windows"',
+      'sec-fetch-dest': 'document',
+      'sec-fetch-mode': 'navigate',
+      'sec-fetch-site': 'none',
+      'sec-fetch-user': '?1',
+      'sec-gpc': '1',
+      'upgrade-insecure-requests': '1',
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
+    },
+    maxRedirects: 20
+  };
 
-      res.on('data', () => {
-        // Do nothing
-      });
+  const req = http.request(options, (res) => {
+    const chunks = [];
 
-      res.on('end', () => {
-        const statusCode = res.statusCode ?? 0;
-        if (statusCode !== 0 && (statusCode < 200 || statusCode >= 400)) {
-          reject(new Error(res.statusMessage));
-          return;
-        }
+    res.on('data', (chunk) => {
+      chunks.push(chunk);
+    });
 
-        resolve();
-      });
-    }).on('error', (error) => {
-      reject(error);
+    res.on('end', (chunk: Uint8Array[]) => {
+      const body = Buffer.concat(chunk);
+      console.log(body.toString());
+    });
+
+    res.on('error', (error) => {
+      console.error(error);
     });
   });
+
+  req.end();
 }
 
 server.listen(80, () => {
