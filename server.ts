891a1d3bc9ae95961269378db09ad40567dd2c2b
Yentis <yentl178@gmail.com>
2023-04-20 17:11:28 +0200

    test impl

--- server.ts
+++ server.ts
@@ -1,6 +1,5 @@
 /* eslint-disable import/first */
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 import * as userLinkingService from './services/userLinkingService';
@@ -8,9 +7,11 @@ import * as settingsService from './services/settingsService';
 import * as osuApiService from './services/osuApiService';
 import { getCountryScores } from './services/buildService';
 import { login } from './services/discordService';
-import express from 'express';
+import http from 'http';
 
-const app = express();
+const server = http.createServer((_, res) => {
+  res.end('Hello, World!');
+});
 
 async function checkToken(): Promise<void> {
   await getCountryScores('53');
@@ -21,6 +22,16 @@ async function init(): Promise<void> {
   const apiKey = process.env.API_KEY;
   if (!apiKey) throw Error('API_KEY environment variable not defined!');
 
+  const pingUrl = process.env.PING_URL;
+  if (pingUrl) {
+    setInterval(() => {
+      console.log('Pinging...');
+      sendRequest(pingUrl).then(() => {
+        console.log('Pinged.');
+      }).catch(console.error);
+    }, 15 * 60 * 1000);
+  }
+
   promises.push(osuApiService.updateBeatmapIds(apiKey));
   promises.push(userLinkingService.start());
   promises.push(settingsService.start());
@@ -30,7 +41,33 @@ async function init(): Promise<void> {
   await checkToken();
 }
 
-app.listen(80, () => {
+async function sendRequest(url: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    http.get(url, (res) => {
+      res.on('error', (error) => {
+        reject(error);
+      });
+
+      res.on('data', () => {
+        // Do nothing
+      });
+
+      res.on('end', () => {
+        const statusCode = res.statusCode ?? 0;
+        if (statusCode !== 0 && (statusCode < 200 || statusCode >= 400)) {
+          reject(new Error(res.statusMessage));
+          return;
+        }
+
+        resolve();
+      });
+    }).on('error', (error) => {
+      reject(error);
+    });
+  });
+}
+
+server.listen(80, () => {
   console.log('Listening on port 80');
 });
 
